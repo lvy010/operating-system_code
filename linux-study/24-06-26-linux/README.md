64df25efa128b704ec946566fb33f790ab3d688f
lvy010 <17338770572@163.com>
2025-12-12 23:11:40 +0800

    Add 24-06-26 process state and wait demos

---
+++ linux-study/24-06-26-linux/README.md
@@ -0,0 +1,4 @@
+# 24-06-26-linux
+
+- Blog: [【Linux详解】进程的状态 | 运行 阻塞 挂起 | 僵尸和孤儿状态](https://lvynote.blog.csdn.net/article/details/139963964)
+
---
+++ linux-study/24-06-26-linux/task2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<assert.h>
+#include<stdlib.h>
+int main()
+{
+    pid_t id=fork();
+    assert(id!=-1);
+    if(id == 0)
+    {
+        execlp("ls","ls","-l","-a",NULL);
+        exit(1);
+    }
+    int status=0;
+    pid_t ret=waitpid(id,&status,0);
+    assert(ret>0);
+    printf("wait success,exitsig=%d,exitCode=%d\n",status&0x7F,(status>>8)&0xFF);
+    return 0;
+}
+
